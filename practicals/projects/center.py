9f440c92b4ef5e3b3ef40b5689a170fd389a751c
Karan Sharma <karansharma1295@gmail.com>
2016-09-30 17:32:58 +0530

    init blueprint

---
+++ practicals/projects/center.py
@@ -0,0 +1,5 @@
+from media import Movie
+
+toy_story = Movie(title = "Toy Story 3", youtube_url ="http://youtube.com",img_url="http://imgur.com",storyline="Crap")
+
+print(toy_story.title)
---
+++ practicals/projects/media.py
@@ -0,0 +1,7 @@
+class Movie:
+	def __init__(self,title,youtube_url,img_url,storyline):
+		self.title = title
+		self.youtube_url = youtube_url
+		self.img_url = img_url
+		self.storyline = storyline
+		

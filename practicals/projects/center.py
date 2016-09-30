226661d306873ddd56e9d3e6183410b3b5e2e40d
Karan Sharma <karansharma1295@gmail.com>
2016-09-30 17:52:21 +0530

    wip

--- practicals/projects/center.py
+++ practicals/projects/center.py
@@ -1,5 +1,10 @@
 from media import Movie
 
-toy_story = Movie(title = "Toy Story 3", youtube_url ="http://youtube.com",img_url="http://imgur.com",storyline="Crap")
+toy_story = Movie(title = "Toy Story 3", youtube_url ="https://www.youtube.com/watch?v=HpeMXwwoZgc",
+				  img_url="http://t3.gstatic.com/images?q=tbn:ANd9GcRUAG6E1nL4GRxsB1G5upnKfQVgm8zeILqd_EbN-2kjMeZZPcah",
+				  storyline='''Andy's toys get mistakenly delivered to a day care centre. 
+				  			Woody convinces the other toys that they weren't dumped and leads them on an expedition back 
+				  			home.''')
 
 print(toy_story.title)
+toy_story.show_trailer()
--- practicals/projects/media.py
+++ practicals/projects/media.py
@@ -1,7 +1,10 @@
+import webbrowser
 class Movie:
 	def __init__(self,title,youtube_url,img_url,storyline):
 		self.title = title
 		self.youtube_url = youtube_url
 		self.img_url = img_url
 		self.storyline = storyline
-		
+
+	def show_trailer(self):
+		webbrowser.open(self.youtube_url)

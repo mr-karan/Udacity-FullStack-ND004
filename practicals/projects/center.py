e94e2cf03a4192ed968eecae7dc191db9fab452a
Karan Sharma <karansharma1295@gmail.com>
2016-09-30 18:03:59 +0530

    more progress

--- practicals/projects/center.py
+++ practicals/projects/center.py
@@ -6,5 +6,11 @@ toy_story = Movie(title = "Toy Story 3", youtube_url ="https://www.youtube.com/w
 				  			Woody convinces the other toys that they weren't dumped and leads them on an expedition back 
 				  			home.''')
 
-print(toy_story.title)
-toy_story.show_trailer()
+
+pulp_fiction = Movie(title = "Pulp Fiction ", youtube_url ="https://www.youtube.com/watch?v=s7EdQ4FqbhY",
+				  img_url="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTZuBLJPU2hkleQzm2iYFQIuVhn_O7gflYziq--y59ov5OAH_SJ4wTr5A",
+				  storyline='''The lives of two mob hit men, a boxer, a gangster's wife, and a pair of diner bandits 
+				  			   intertwine in four tales of violence and redemption''')
+
+print("Fav movie is"+pulp_fiction.title)
+pulp_fiction.show_trailer()

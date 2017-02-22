860316ba95f58e7fde2af1fd5bd02929404d8093
Karan Sharma <karansharma1295@gmail.com>
2017-02-22 19:54:49 +0530

    fix bugs

--- Project5/application.py
+++ Project5/application.py
@@ -1,24 +1,26 @@
 import os
 from flask import Flask, flash, g, jsonify, redirect, render_template,\
-                  request, session, url_for
+    request, session, url_for
 from flask.ext.github import GitHub
-
-app = Flask(__name__)
-app.secret_key = "thisisasecretkey"
-
-if os.environ.has_key('GITHUB_CLIENT_ID') and\
-   os.environ.has_key('GITHUB_CLIENT_SECRET'):
-    app.config['GITHUB_CLIENT_ID'] = os.environ['GITHUB_CLIENT_ID']
-    app.config['GITHUB_CLIENT_SECRET'] = os.environ['GITHUB_CLIENT_SECRET']
-else:
-    app.config['GITHUB_CLIENT_ID'] = None
-    app.config['GITHUB_CLIENT_SECRET'] = None
-github = GitHub(app)
-
 from database_setup import Base, User, Artist, Song
 from datetime import datetime
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
+
+
+app = Flask(__name__)
+app.secret_key = "thisisasecretkey"
+
+
+if os.environ['GITHUB_CLIENT_ID'] and os.environ['GITHUB_CLIENT_SECRET']:
+    app.config['GITHUB_CLIENT_ID'] = os.environ['GITHUB_CLIENT_ID']
+    app.config['GITHUB_CLIENT_SECRET'] = os.environ['GITHUB_CLIENT_SECRET']
+
+else:
+    app.config['GITHUB_CLIENT_ID'] = None
+    app.config['GITHUB_CLIENT_SECRET'] = None
+
+github = GitHub(app)
 
 engine = create_engine('sqlite:///catalog.db')
 Base.metadata.bind = engine
@@ -30,15 +32,20 @@ repo_uri = 'https://github.com/mr-karan/Udacity-FullStack-ND004'
 base_uri = '/catalog/'
 api_uri = base_uri + 'api/'
 
+
 # oauth
 @app.route('/login')
 def login():
     """ login route/routine """
     if app.config['GITHUB_CLIENT_ID'] and app.config['GITHUB_CLIENT_SECRET']:
-        return github.authorize(redirect_uri=url_for('authorized', _external=True))
+        return github.authorize(
+            redirect_uri=url_for(
+                'authorized',
+                _external=True))
     else:
         flash('missing Github API Client ID.', 'warning')
         return redirect(url_for('index'))
+
 
 @app.route('/logout')
 def logout():
@@ -46,6 +53,7 @@ def logout():
     session.pop('access_token', None)
     flash('Logged Out.', 'success')
     return redirect(url_for('index'))
+
 
 @app.route('/github-callback')
 @github.authorized_handler
@@ -69,6 +77,7 @@ def authorized(oauth_token):
     flash('You\'re logged in! ', 'success')
     return redirect(url_for('index'))
 
+
 # helper functions
 def base_query():
     """ returns the full list of artists and songs """
@@ -76,37 +85,41 @@ def base_query():
     songs = db_session.query(Song).all()
     return artists, songs
 
+
 def parse_song_form(form):
     """ returns a new Song object from submitted form data POST requests """
     form = dict(form)
-    if not form.has_key('song-featured'):
+    if 'song-featured' not in form:
         form['song-featured'] = [False]
     else:
         form['song-featured'] = [True]
     song = Song(name=form['song-name'][0],
-                    song_url=form['song-url'][0],
-                    description=form['song-description'][0],
-                    featured=form['song-featured'][0],
-                    artist_id=form['song-artist'][0])
+                song_url=form['song-url'][0],
+                description=form['song-description'][0],
+                featured=form['song-featured'][0],
+                artist_id=form['song-artist'][0])
     return song
+
 
 def authenticated():
     """ returns whether or not the session user is authenticated """
-    if session.has_key('user_id') and session.has_key('user_token'):
+    if 'user_id' in session and 'user_token' in session:
         user = db_session.query(User).filter_by(id=session['user_id']).first()
         if user:
             return user.access_token == session['user_token']
     return False
 
+
 def can_edit(song):
     """ returns whether the song is owned by the session user """
     if authenticated():
-        if song.adder_id == None:
+        if song.adder_id is None:
             return False
         else:
-            return session.has_key('user_id') and song.adder_id == session['user_id']
+            return 'user_id' in session and song.adder_id == session['user_id']
     else:
         return False
+
 
 # routes
 @app.route('/source')
@@ -114,14 +127,15 @@ def source():
     """ redirects to github repository """
     return redirect(repo_uri)
 
-@app.route(base_uri+'seed')
+
+@app.route(base_uri + 'seed')
 def seed_database(fixture_filename='fixtures.json'):
     artists, _ = base_query()
     if len(artists) != 0:
         pass
     else:
         import json
-        with open(fixture_filename) as data_file:    
+        with open(fixture_filename) as data_file:
             fixtures = json.load(data_file)
             print(fixtures)
         seed_artists = fixtures['artists']
@@ -131,10 +145,10 @@ def seed_database(fixture_filename='fixtures.json'):
         seed_songs = fixtures['songs']
         for c in seed_songs:
             song = Song(name=c['name'],
-                            song_url=c['song_url'],
-                            description=c['description'],
-                            featured=c['featured'],
-                            artist_id=c['artist_id'])
+                        song_url=c['song_url'],
+                        description=c['description'],
+                        featured=c['featured'],
+                        artist_id=c['artist_id'])
             db_session.add(song)
         try:
             db_session.commit()
@@ -142,6 +156,7 @@ def seed_database(fixture_filename='fixtures.json'):
         except Exception as e:
             flash('error. {}'.format(e), 'danger')
     return redirect(url_for('index'))
+
 
 @app.route('/catalog')
 @app.route('/')
@@ -154,19 +169,22 @@ def index():
                            title='Featured Songs', title_link=None,
                            logged_in=authenticated, editable=can_edit)
 
-@app.route(api_uri+'artists', methods=['GET'])
+
+@app.route(api_uri + 'artists', methods=['GET'])
 def index_artists_api():
     """ returns JSON response of artists """
     artists, _ = base_query()
     return jsonify(artists=[p.serialize for p in artists])
 
-@app.route(api_uri+'artists/<int:artist_id>', methods=['GET'])
+
+@app.route(api_uri + 'artists/<int:artist_id>', methods=['GET'])
 def index_songs_api(artist_id):
     """ returns JSON response of songs """
     artist_songs = db_session.query(Song).filter_by(artist_id=artist_id)
     return jsonify(songs=[pc.serialize for pc in artist_songs])
 
-@app.route(base_uri+'artists/<int:artist_id>', methods=['GET'])
+
+@app.route(base_uri + 'artists/<int:artist_id>', methods=['GET'])
 def index_songs(artist_id):
     """ artist show screen / songs index screen """
     artists, _ = base_query()
@@ -181,7 +199,8 @@ def index_songs(artist_id):
                            title=artist.name,
                            logged_in=authenticated, editable=can_edit)
 
-@app.route(base_uri+'songs/<int:song_id>', methods=['GET'])
+
+@app.route(base_uri + 'songs/<int:song_id>', methods=['GET'])
 def view_song(song_id):
     """ song view screen """
     artists, _ = base_query()
@@ -196,7 +215,8 @@ def view_song(song_id):
                            logged_in=authenticated,
                            editable=can_edit)
 
-@app.route(base_uri+'songs/new', methods=['GET', 'POST'])
+
+@app.route(base_uri + 'songs/new', methods=['GET', 'POST'])
 def new_song():
     """ handles new song creation """
     if not authenticated():
@@ -215,8 +235,8 @@ def new_song():
             return redirect(url_for('index'))
     else:
         song = {"id": None, "name": "", "song_url": "",
-                  "song_number": "", "description": "", "start_date": "",
-                  "featured": False, "artist_id": None}
+                "description": "", "start_date": "",
+                "featured": False, "artist_id": None}
         return render_template('edit_song.html',
                                artists=artists, song=song,
                                title='New Song',
@@ -224,7 +244,7 @@ def new_song():
                                logged_in=authenticated)
 
 
-@app.route(base_uri+'songs/<int:song_id>/delete', methods=['GET', 'POST'])
+@app.route(base_uri + 'songs/<int:song_id>/delete', methods=['GET', 'POST'])
 def delete_song(song_id):
     """ handles song deletion """
     artists, _ = base_query()
@@ -250,7 +270,8 @@ def delete_song(song_id):
                            form_action=url_for('delete_song', song_id=song_id),
                            logged_in=authenticated)
 
-@app.route(base_uri+'songs/<int:song_id>/edit', methods=['GET', 'POST'])
+
+@app.route(base_uri + 'songs/<int:song_id>/edit', methods=['GET', 'POST'])
 def edit_song(song_id):
     """ handles song editing """
     artists, _ = base_query()
@@ -258,15 +279,15 @@ def edit_song(song_id):
     if not authenticated():
         return redirect(url_for('login'))
     elif not can_edit(song):
-        flash('You don\'t own this! (Can\'t touch this, dun nan nan na... na na... na na.)', 'warning')
+        flash(
+            'You don\'t own this! (Can\'t edit)',
+            'warning')
         return redirect(url_for('view_song', song_id=song_id))
     if request.method == 'POST':
         song_params = parse_song_form(request.form)
         song.name = song_params.name
         song.song_url = song_params.song_url
-        song.song_number = song_params.song_number
         song.description = song_params.description
-        song.start_date = song_params.start_date
         song.featured = song_params.featured
         song.artist_id = song_params.artist_id
         db_session.add(song)
@@ -277,11 +298,15 @@ def edit_song(song_id):
             flash('Something imploded. {}'.format(e), 'danger')
         return redirect(url_for('view_song', song_id=song_id))
     else:
-        return render_template('edit_song.html',
-                               artists=artists, song=song,
-                               title='Editing: ' + song.name,
-                               form_action=url_for('edit_song', song_id=song_id),
-                               logged_in=authenticated)
+        return render_template(
+            'edit_song.html',
+            artists=artists,
+            song=song,
+            title='Editing: ' + song.name,
+            form_action=url_for(
+                'edit_song',
+                song_id=song_id),
+            logged_in=authenticated)
 
 
 if __name__ == '__main__':
